cda37dcfda672de12d2ff5f73a7d4b3b0aed0c48
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-11-12 18:08:32 -0800

    routes in place

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API'}))
 
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
 
 const PORT = process.env.PORT || 5000
 
