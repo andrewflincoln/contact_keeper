3a0bdd780f56f8acd2c471755d242795125319bf
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-11-20 14:28:50 -0800

    update contact

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,8 +1,23 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import ContactContext from '../../context/contact/contactContext'
 
 const ContactForm = () => {
   const contactContext = useContext(ContactContext)
+
+  const { addContact, clearCurrent, current, updateContact } = contactContext
+
+  useEffect(() => {
+    if (current != null) {
+      setContact(current)
+    } else {
+      setContact({
+        name: '',
+        email: '',
+        phone: '',
+        type: 'personal'
+      })
+    }
+  }, [contactContext, current])
 
   const [contact, setContact] = useState({
     name: '',
@@ -17,7 +32,13 @@ const ContactForm = () => {
 
   const onSubmit = e => {
     e.preventDefault()
-    contactContext.addContact(contact)
+
+    if (current === null) {
+      addContact(contact)
+    } else {
+      updateContact(contact)
+    }
+    
     setContact({
       name: '',
       email: '',
@@ -26,10 +47,14 @@ const ContactForm = () => {
     })
   }
 
+  const clearAll = () => {
+    clearCurrent()
+
+  }
 
   return (
     <form onSubmit={onSubmit}>
-      <h2 className="text-primary">Add Contact</h2>
+      <h2 className="text-primary">{current ? 'Edit Contact' : 'Add Contact'}</h2>
       <input type="text" placeholder="Name" name="name" value={name} onChange={onChange}/>
       <input type="email" placeholder="Email" name="email" value={email} onChange={onChange}/>
       <input type="phone" placeholder="Phone" name="phone" value={phone} onChange={onChange}/>
@@ -38,7 +63,11 @@ const ContactForm = () => {
       <input type="radio" name="type" value="personal" checked={type==='personal'}/>Personal {' '}
       <input type="radio" name="type" value="professional" checked={type==='professional'} onChange={onChange}/>Professional
 
-      <input type="submit" value="Add Contact" className="btn btn-primary btn-block" onChange={onChange}/>
+      <input type="submit" value={current ? 'Update Contact' : "Add Contact"} className="btn btn-primary btn-block" onChange={onChange}/>
+
+      {current && <div>
+        <button className="btn btn-light btn-block" onClick={clearAll}>Clear</button>
+      </div>}
     </form>
   )
 }
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -34,7 +34,9 @@ export default (state, action) => {
     case UPDATE_CONTACT:
       return {
         ...state,
-
+        contacts: state.contacts.map(contact => 
+          contact.id === action.payload.id ? action.payload : contact
+        )
       }
 
       default: 
