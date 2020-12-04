6f547a1dad7dd4e87527213ac33633ffae8c9764
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-12-03 21:42:34 -0800

    full crud

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,9 +62,38 @@ const ContactState = props => {
   }
 
   //Delete Contact
-  const deleteContact = contact => {
-    dispatch({ type: DELETE_CONTACT, payload: contact})
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`)
+      dispatch({ type: DELETE_CONTACT, payload: id})
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR,
+      payload: err.response.msg })
+    }
+   
   }
+
+  //Update contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
+      dispatch({ type: UPDATE_CONTACT, payload: res.data})
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR,
+      payload: err.response.msg })
+    }
+
+    
+  }
+
+    
+
 
   // Clear Contacts
   const clearContacts = () => {
@@ -81,10 +110,6 @@ const ContactState = props => {
     dispatch({ type: CLEAR_CURRENT})
   }
 
-  //Update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact})
-  }
 
   //Filter contacts
   const filterContacts = text => {
